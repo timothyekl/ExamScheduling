3bc0a4a9c4ea155df648c9780bcd473dc9c8ccbf
Tim Ekl <lithium3141@gmail.com>
2011-02-03 14:34:05 -0500

    Finish project

--- src/Processor.java
+++ src/Processor.java
@@ -60,9 +60,14 @@ public class Processor {
 			classSchedule[i] = new ArrayList<Section>();
 		}
 		IterateThroughClasses(true, true, classSchedule, NumPeriods-2, NumRooms);
-		IterateThroughClasses(false, false, classSchedule, NumPeriods, NumRooms);
+		PostProcessor(classSchedule, NumRooms, NumPeriods-2);
+		IterateThroughClasses(false, true, classSchedule, NumPeriods-1, NumRooms);
+		PostProcessor(classSchedule, NumRooms, NumPeriods-1);
+		IterateThroughClasses(false, true, classSchedule, NumPeriods, NumRooms);
+		PostProcessor(classSchedule, NumRooms, NumPeriods);
 		return classSchedule;
 	}
+	
 	public void IterateThroughClasses(boolean SkipHumanities, boolean CheckThrees, ArrayList<Section>[] classSchedule, int NumPeriods, int NumRooms)
 	{
 		for(int i=0;i<Classes.size();++i)
@@ -71,9 +76,7 @@ public class Processor {
 			if(SkipHumanities)
 			{
 				if(Classes.get(i).id.startsWith("SL") ||
-				   Classes.get(i).id.startsWith("JP") ||
-				   Classes.get(i).id.startsWith("GL") ||
-				   Classes.get(i).id.startsWith("EMGT")) continue;
+				   Classes.get(i).id.startsWith("JP")) continue;
 			}
 			for(int j=0;j<NumPeriods;++j)
 			{ // for each period
@@ -122,6 +125,90 @@ public class Processor {
 			}
 		}
 	}
+	public ArrayList<Section> GetOrphans()
+	{
+		ArrayList<Section> orphans = new ArrayList<Section>();
+		for(int i=0;i<Classes.size();++i)
+		{
+			if(Classes.get(i).ExamPeriod==-1)
+			{
+				orphans.add(Classes.get(i));
+			}
+		}
+		ArrayList<Section> returns = new ArrayList<Section>();
+		ArrayList<Section> candidates = new ArrayList<Section>();
+		for(int i=0;i<orphans.size();++i)
+		{
+			//if(orphans.get(i).id.equals("SL151") || orphans.get(i).id.equals("GL366")) continue;
+			candidates.clear();
+			candidates.add(orphans.get(i));
+			for(int j=i+1;j<orphans.size();++j)
+			{
+				boolean usable = true;
+				for(int k=0;k<candidates.size();++k)
+				{
+					if(candidates.get(k)!=orphans.get(j) && intersection(orphans.get(j).students, candidates.get(k).students).size()!=0) usable = false;
+				}
+				
+				if(orphans.get(i) == orphans.get(j))
+				{
+					candidates.add(orphans.get(j));
+				}
+				else
+				{
+					
+					if(usable) candidates.add(orphans.get(j));
+				}
+			}
+			if(candidates.size() > returns.size()) 
+			{
+				returns = (ArrayList<Section>)candidates.clone();
+			}
+		}
+		System.out.println();
+		return returns;
+	}
+	public void PostProcessor(ArrayList<Section>[] schedule, int NumRooms, int NumPeriods)
+	{
+		ArrayList<Section> orphans = GetOrphans();
+		for(int p = 0;p<NumPeriods;++p)
+		{
+			if(orphans.size()==0) break;
+			ArrayList<Section> ClassesToSwitch = new ArrayList<Section>();
+			for(int i=0;i<orphans.size();++i)
+			{
+				for(int j=0;j<schedule[p].size();++j)
+				{
+					ArrayList<Student> intersect = intersection(orphans.get(i).students, schedule[p].get(j).students);
+					if(intersect.size() != 0)
+					{
+						if(!ClassesToSwitch.contains(schedule[p].get(j)))
+							for(int k=0;k<schedule[p].get(j).professors.size();++k) 
+							{ ClassesToSwitch.add(schedule[p].get(j)); }
+					}
+				}
+			}
+			if(ClassesToSwitch.size()<=orphans.size() && NumRooms >= schedule[p].size()-ClassesToSwitch.size()+orphans.size())
+			{
+				System.out.println("Swapping "+ClassesToSwitch.size()+" classes with "+orphans.size()+" classes in period "+(p+1));
+				for(int i=0;i<ClassesToSwitch.size();++i)
+				{
+					System.out.println("\tRemoving "+ClassesToSwitch.get(i).id+" ("+i+")");
+					while(schedule[p].remove(ClassesToSwitch.get(i))) {};
+					ClassesToSwitch.get(i).ExamPeriod = -1;
+				}
+				for(int i=0;i<orphans.size();++i)
+				{
+					System.out.println("\tAdding "+orphans.get(i).id+" ("+i+")");
+					schedule[p].add(orphans.get(i));
+					orphans.get(i).ExamPeriod = p;
+				}
+				orphans = GetOrphans();
+				p = 0;
+			}
+			
+		}
+	}
 	private void DisplaySchedule(ArrayList<Section>[] schedule)
 	{
 		for(int i=0;i<schedule.length;++i)
@@ -135,6 +222,7 @@ public class Processor {
 		}
 		for(int i=0;i<Classes.size();++i)
 		{
+			if(i>0 && Classes.get(i)==Classes.get(i-1)) continue;
 			if(Classes.get(i).ExamPeriod==-1)
 			{
 				System.out.print("Class "+Classes.get(i).id+" was not scheduled.\n\t");
@@ -144,6 +232,21 @@ public class Processor {
 					System.out.print(s1.id+" ");
 				}
 				System.out.println();
+				int p = 10;
+				for(int j=0;j<schedule[p].size();++j)
+				{
+					if(j>0 && schedule[p].get(j)==schedule[p].get(j-1)) continue;
+					ArrayList<Student> intersect = intersection(Classes.get(i).students, schedule[p].get(j).students);
+					if(intersect.size() != 0)
+					{
+						System.out.print("\t"+schedule[p].get(j).id+": ");
+						for(int k=0;k<intersect.size();++k)
+						{
+							System.out.print(intersect.get(k).id+" ");
+						}
+						System.out.println();
+					}
+				}
 			}
 		}
 		for(int i=0;i<Students.size();++i)
@@ -167,5 +270,32 @@ public class Processor {
 				}
 			}
 		}
+	}
+	static <E> ArrayList<E> intersection(List<E> A1, List<E> A2)
+	{
+		ArrayList<E> Rt = new ArrayList<E>();
+		for(int i=0;i<A1.size();++i)
+		{
+			for(int j=0;j<A2.size();++j)
+			{
+				if(A1.get(i).equals(A2.get(j))) Rt.add(A1.get(i));
+			}
+		}
+		return Rt;
+		
+	}
+	static <E> ArrayList<E> union(List<E> A1, List<E> A2)
+	{
+		ArrayList<E> Rt = new ArrayList<E>();
+		for(int i=0;i<A1.size();++i)
+		{
+			Rt.add(A1.get(i));
+		}
+		for(int j=0;j<A2.size();++j)
+		{
+			if(!Rt.contains((A2.get(j)))) Rt.add(A2.get(j));
+		}
+		return Rt;
+		
 	}
 }
